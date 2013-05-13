3a3253ee4c00325febbbbe55d16d4d38b701f99e
woxxy <woxxy@foolrulez.org>
2013-05-13 14:33:50 +0200

    Fixing URI for save file, the file is still not appearing

--- src/net/analogyc/wordiary/MainActivity.java
+++ src/net/analogyc/wordiary/MainActivity.java
@@ -32,6 +32,7 @@ implements NewEntryDialogFragment.NewEntryDialogListener{
 	//view links
 	private Button entryButton;
 	private ListView entryList;
+	private Uri fileUri;
 	
 	DataBaseHelper dataBaseHelper;
 	
@@ -101,7 +102,8 @@ implements NewEntryDialogFragment.NewEntryDialogListener{
     //method used when "takePhoto" button is clicked
     public void onTakePhotoClicked(View view){
     	Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);    	
-    	intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(Photo.getOutputMediaFile(1)));
+    	fileUri = Uri.fromFile(Photo.getOutputMediaFile(1));
+    	intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
     	startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
     }
     
@@ -110,8 +112,7 @@ implements NewEntryDialogFragment.NewEntryDialogListener{
         if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
                 // Image captured and saved to fileUri specified in the Intent
-                Toast.makeText(this, "Image saved to:\n" +
-                         data.getData(), Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Image saved to:\n" + fileUri, Toast.LENGTH_LONG).show();
             } else if (resultCode == RESULT_CANCELED) {
                 // User cancelled the image capture
             } else {

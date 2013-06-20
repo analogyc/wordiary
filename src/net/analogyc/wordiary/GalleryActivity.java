e5e088b043859b5c7535d562bf2a977ff7c0c589
Bettapro <alberto3bettin@gmail.com>
2013-06-20 15:01:17 +0200

    image in the gallery permits to launch Image Activity.

--- src/net/analogyc/wordiary/GalleryActivity.java
+++ src/net/analogyc/wordiary/GalleryActivity.java
@@ -27,9 +27,10 @@ public class GalleryActivity extends BaseActivity {
 		gridView.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
-				// TODO Auto-generated method stub
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,long arg3) {
+				Intent intent = new Intent(GalleryActivity.this, ImageActivity.class);
+				intent.putExtra("dayId", (int)arg3);
+				startActivity(intent);
 				
 			}
 		});

32f50a80e42b243811a957719d0354447370c3d6
woxxy <woxxy@foolrulez.org>
2013-06-23 13:40:25 +0200

    Implemented fling to change image

--- src/net/analogyc/wordiary/ImageActivity.java
+++ src/net/analogyc/wordiary/ImageActivity.java
@@ -4,15 +4,11 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.view.Display;
-import android.view.Menu;
-import android.view.WindowManager;
+import android.util.Log;
+import android.view.*;
 import net.analogyc.wordiary.models.DBAdapter;
-
-import java.io.InputStream;
 
 public class ImageActivity extends Activity {
 	private int dayId;
@@ -33,21 +29,54 @@ public class ImageActivity extends Activity {
 		imageWebView = (ImageWebView) findViewById(R.id.imageWebView);
 	}
 
+	public void getNext(boolean backwards) {
+		Cursor c = dataBase.getNextDay(dayId, backwards);
+		if (c.getCount() == 1) {
+			c.moveToNext();
+			dayId = c.getInt(0);
+			setView();
+		}
+	}
+
 
 	public void setView() {
-		InputStream imageStream;
-		Bitmap image;
 		String location;
 		if (dayId == -1) {
-			location = "file://android_asset/default-avatar.jpg";
+			location = "file:///android_asset/default-avatar.jpg";
 		} else {
 			Cursor c = dataBase.getDayById(dayId);
 			c.moveToFirst();
-			location = "file://" + c.getString(1);
+			if (c.getString(1) == "") {
+				location = "file:///android_asset/default-avatar.jpg";
+			} else {
+				location = "file://" + c.getString(1);
+			}
 			c.close();
 		}
 
 		imageWebView.setImage(location);
+
+		// all custom onFlingListener for ImageWebView
+		imageWebView.setOnFlingListener(new ImageWebView.OnFlingListener() {
+			@Override
+			public boolean onFling(View view, MotionEvent e1, MotionEvent motionEvent, float velocityX, float velocityY) {
+				Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+				DisplayMetrics dm = new DisplayMetrics();
+				display.getMetrics(dm);
+
+				if ((dm.densityDpi > 300 && imageWebView.getScale() == 2.0f)
+					|| dm.densityDpi <= 300 && imageWebView.getScale() == 1.0f) {
+
+					if (velocityX > 1000f) {
+						getNext(false);
+					} else if (velocityX < -1000f) {
+						getNext(true);
+					}
+				}
+
+				return true;
+			}
+		});
 	}
 
 	@Override
